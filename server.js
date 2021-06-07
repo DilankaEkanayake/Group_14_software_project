79bca07d23d3ff6f6f3069d8f49230d9a99046ad
DilankaEkanayake <ekanayakedilanka4@gmail.com>
2021-06-08 01:47:01 +0530

    dep

--- server.js
+++ server.js
@@ -21,14 +21,14 @@ app.use('/api/auth', require('./routes/api/auth'));
 //server static assests if in production
 if(process.env.NODE_ENV ==='production') {
     // set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, '/client/build')));
 
     app.get('*', (req, res) => {
-       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
-    });
+       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); 
+    })
 }
 else{
-    app.get('/', (req, res) => { res.send('API Running');
+    app.get("/", (req, res) => { res.send('API Running');
 });
 }
 const PORT = process.env.PORT || 5000;
